5c0b93b3039fcc6bcae277faa228858b5a16fc6e
Gabriel Bissonnette <gabrielbissonnette36@gmail.com>
2023-04-24 21:39:37 -0400

    Update website

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h4>Essential Links</h4>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<v-card class="mx-auto px-6 py-8 elevation-0" max-width="90%">
+<h3>#UnComaPourManquerLesCours</h3>
+    <h1>Apprends LE SORT qui fait fureur partout dans le Chemin de Traverse.</h1>
+<br>
+  <v-card color="teal-darken-3">
+    <h1>Ton Compte</h1>
+    <v-form v-model="form" @submit.prevent="onSubmit">
+
+      <v-text-field v-model="password" :readonly="loading" :rules="[required]" clearable label="Mot de passe"
+        :type="show1 ? 'text' : 'password'" placeholder="Entrez votre mot de passe"></v-text-field>
+
+      <v-btn :disabled="!form" :loading="loading" block color="teal-darken-4" rounded="pill" size="large" type="submit" variant="elevated">
+        Se Connecter
+      </v-btn>
+    </v-form>
+  </v-card>
+
+  <v-dialog
+      v-model="dialog"
+      width="auto"
+    >
+      <v-card>
+        <v-card-text>
+          Mot de passe incorrect.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="teal-darken-4" block @click="dialog = false">Fermer</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-card>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+
+  data: () => ({
+    form: false,
+    show1: false,
+    password: "",
+    confirmPassword: "test",
+    loading: false,
+
+    dialog: false,
+  }),
+  methods: {
+    onSubmit() {
+      if (!this.form) return
+
+      
+
+      if (this.password === "test") {
+        this.loading = true;
+
+        setTimeout(() => (this.loading = false), 1000)
+      }
+      else{
+        this.dialog = true;
+      }
+
+
+
+    },
+    required(v) {
+      return !!v || 'Field is required'
+    },
+
+  },
+  computed: {
+    passwordConfirmationRule() {
+      return () =>
+        this.password === this.confirmPassword || "Password must match";
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
